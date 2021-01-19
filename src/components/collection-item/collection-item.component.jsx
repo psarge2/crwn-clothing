e9649fa6a3a7f4f1b683209e42afde3c54751f91
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-01-19 13:10:30 -0500

    added collection-item component

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -9,8 +9,12 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       style={{ 
         backgroundImage: `url(${imageUrl})`
        }}
-    >
-
+      />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
   </div>
 )
+
+export default CollectionItem;
