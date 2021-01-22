412514cc33a08f42d90d7c605cfdd0d94a3ae5fb
psarge2 <54369285+psarge2@users.noreply.github.com>
2021-01-22 11:10:42 -0500

    initial settup for cart button and dropdown components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -26,7 +29,9 @@ const Header = ({ currentUser }) => (
       )  : (
         <Link className='option' to='/signin'> SIGN IN </Link>
       )}
+      <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 )
 
